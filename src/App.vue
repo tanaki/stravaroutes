9c2f81d9a829b8070fb28b0afaa4f4fc02b73485
nicolas pigelet <nicolas.pigelet@cri-paris.org>
2021-01-19 23:16:03 +0100

    fetch list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <a v-if="(!athlete && !code)" :href="connectionPage">
+    <img src="./assets/btn_strava_connectwith_orange.svg" />
+  </a>
+
+  <div v-if="athlete && !routes">Loading</div>
+
+  <div v-if="routes" class="">
+    <List :routes="routes" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue'
 
 export default {
   name: 'App',
+
+  data () {
+
+    return {
+      athlete: null,
+      code: null,
+      connectionPage: "https://www.strava.com/oauth/authorize?client_id=242&response_type=code&redirect_uri=http://nicolaspigelet.com/strava/routes&approval_prompt=force",
+      routes: null
+    }
+  },
+
   components: {
-    HelloWorld
+    List
+  },
+
+  mounted () {
+
+    var self = this;
+    window.location.search.split('&').forEach(( s ) => {
+      var splitted = s.split('=')
+      if ( splitted[0] == 'code' ) {
+        self.code = splitted[1];
+        self.fetchAthlete()
+      }
+    });
+  },
+
+  methods: {
+    
+    fetchAthlete () {
+
+      var self = this;
+
+      const requestOptions = {
+        method: "GET",
+        headers: { 
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + this.code
+        }
+      };
+
+      fetch( "https://www.strava.com/api/v3/athlete", requestOptions)
+        .then(response => response.json())
+        .then(data => {
+
+          self.athlete = data
+          self.fetchRoutes();
+
+        });
+
+    },
+
+    fetchRoutes () {
+
+      var self = this;
+
+      const requestOptions = {
+        method: "GET",
+        headers: { 
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + this.code
+        }
+      };
+
+      fetch( "https://www.strava.com/api/v3/athletes/" + this.athlete.id + "/routes?per_page=200", requestOptions)
+        .then(response => response.json())
+        .then(data => {
+
+          console.log( data )
+          self.routes = data
+
+        });
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,54 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th @click="sort('name')">Name</th>
+        <th @click="sort('distance')">Distance</th>
+        <th @click="sort('elevation_gain')">D+</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="route in routes" :key="route.id">
+        <td>{{route.name}}</td>
+        <td>{{(route.distance / 1000).toFixed(2)}}km</td>
+        <td>{{Math.ceil(route.elevation_gain)}}m</td>
+        <td>{{route.type}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    props: {
+      routes: Array
+    },
+    data() {
+      return {
+        currentSort:'name',
+        currentSortDir:'asc'
+      }
+    },
+    computed: {
+      
+    },
+    mounted() {
+      
+    },
+    methods: {
+      
+      sort:function(s) {
+
+        //if s == current sort, reverse
+        if(s === this.currentSort) {
+          this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+        }
+        this.currentSort = s;
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
