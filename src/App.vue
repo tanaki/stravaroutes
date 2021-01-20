f27a61e296dc0224c9f46080a0c13ec23ea2af62
nicolas pigelet <nicolas.pigelet@cri-paris.org>
2021-01-20 22:08:58 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
 
-  <a v-if="(!athlete && !code)" :href="connectionPage">
-    <img src="./assets/btn_strava_connectwith_orange.svg" />
-  </a>
+  <div class="loggedout" v-if="(!athlete && !code)">
+    <h3>Strava Routes</h3>
+    <a :href="connectionPage">
+      <img src="./assets/btn_strava_connectwith_orange.svg" />
+    </a>
+  </div>
 
   <div v-if="athlete && !routes">Loading</div>
 
   <div v-if="routes" class="">
+
     <List :routes="routes" />
   </div>
 </template>
 
 <script>
+
 import List from './components/List.vue'
 
 export default {
@@ -23,7 +28,8 @@ export default {
       athlete: null,
       code: null,
       connectionPage: "https://www.strava.com/oauth/authorize?client_id=242&response_type=code&redirect_uri=http://nicolaspigelet.com/strava/routes&approval_prompt=force",
-      routes: null
+      routes: null,
+      token: null
     }
   },
 
@@ -38,12 +44,49 @@ export default {
       var splitted = s.split('=')
       if ( splitted[0] == 'code' ) {
         self.code = splitted[1];
-        self.fetchAthlete()
+        self.fetchToken()
+
+        // self.token = splitted[1];
+        // self.fetchAthlete();
       }
     });
   },
 
   methods: {
+
+    fetchToken () {
+
+      var self = this;
+
+      const requestOptions = {
+        method: "POST",
+        headers: { 
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          client_id: 242,
+          client_secret: "1156bdd01e5679547af4ac46bce0722964704f06",
+          code: self.code,
+          grant_type: "authorization_code"
+        })
+      };
+
+      fetch( "https://www.strava.com/oauth/token", requestOptions)
+        .then(response => response.json())
+        .then(data => {
+
+          if ( data.errors ) {
+
+            console.log("error");
+
+          } else {
+
+            self.token = data.access_token
+            self.fetchAthlete();
+          }
+
+        });
+    },
     
     fetchAthlete () {
 
@@ -53,7 +96,7 @@ export default {
         method: "GET",
         headers: { 
           "Content-Type": "application/json",
-          "Authorization": "Bearer " + this.code
+          "Authorization": "Bearer " + this.token
         }
       };
 
@@ -61,8 +104,15 @@ export default {
         .then(response => response.json())
         .then(data => {
 
-          self.athlete = data
-          self.fetchRoutes();
+          if ( data.errors ) {
+
+            console.log("error");
+
+          } else {
+
+            self.athlete = data
+            self.fetchRoutes();
+          }
 
         });
 
@@ -76,7 +126,7 @@ export default {
         method: "GET",
         headers: { 
           "Content-Type": "application/json",
-          "Authorization": "Bearer " + this.code
+          "Authorization": "Bearer " + this.token
         }
       };
 
@@ -84,8 +134,15 @@ export default {
         .then(response => response.json())
         .then(data => {
 
-          console.log( data )
-          self.routes = data
+          if ( data.errors ) {
+
+            console.log("error");
+
+          } else {
+
+            console.log( data )
+            self.routes = data
+          }
 
         });
     }
@@ -95,4 +152,65 @@ export default {
 
 <style>
 
+@import './assets/reset.css';
+
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#app {
+  padding-top: 40px;
+}
+
+h3 {
+  font-weight: bold;
+}
+.loggedout h3 {
+  margin-bottom: 40px;
+}
+
+.search {
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  max-width: 800px;
+  margin: 0 auto;
+}
+.search input {
+  -webkit-appearance: none;
+  appearance: none;
+  border-color: #F3F3F3;
+  border-style: solid;
+  padding: 10px;
+}
+
+table {
+  border: 1px solid #F3F3F3;
+  border-spacing: 0;
+  margin: 20px auto;
+  max-width: 800px;
+  width: 800px;
+}
+tr {
+  border: none;
+}
+th,
+td {
+  border: none;
+  padding: 10px;
+}
+th {
+  border-bottom: 1px solid #F3F3F3;
+  cursor: pointer;
+  font-weight: bold;
+}
+.name {
+  text-align: left;
+}
+tr:nth-child(even) {
+  background: #F9F9F9;
+}
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,18 +1,27 @@
 <template>
+
+  <div class="search">
+    <h3>Strava Routes</h3>
+    <input id="search" type="search" placeholder="Search" v-model="searchValue">
+  </div>
+
   <table>
     <thead>
       <tr>
-        <th @click="sort('name')">Name</th>
+        <th class="name" @click="sort('name')">Name</th>
         <th @click="sort('distance')">Distance</th>
         <th @click="sort('elevation_gain')">D+</th>
+        <th @click="sort('type')">Type</th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="route in routes" :key="route.id">
-        <td>{{route.name}}</td>
-        <td>{{(route.distance / 1000).toFixed(2)}}km</td>
-        <td>{{Math.ceil(route.elevation_gain)}}m</td>
-        <td>{{route.type}}</td>
+      <tr v-for="route in sortedRoutes" :key="route.id">
+        <td class="name">
+          <a :href="'https://www.strava.com/routes/'+route.id_str" target="_blank">{{route.name}}</a>
+        </td>
+        <td class="distance">{{(route.distance / 1000).toFixed(2)}}km</td>
+        <td class="elevation">{{Math.ceil(route.elevation_gain)}}m</td>
+        <td class="type">{{type[route.type]}}</td>
       </tr>
     </tbody>
   </table>
@@ -26,12 +35,34 @@
     data() {
       return {
         currentSort:'name',
-        currentSortDir:'asc'
+        currentSortDir:'asc',
+        searchValue: "",
+        type: ['', 'bike', 'run', 'other']
       }
     },
-    computed: {
-      
+
+    computed:{
+      sortedRoutes() {
+
+        var self = this;
+
+        if (self && self.routes && self.routes.length > 0)
+
+          return this.routes.slice().sort((a,b) => {
+            let modifier = 1;
+            if(this.currentSortDir === 'desc') modifier = -1;
+            if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+            if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+            return 0;
+          }).filter( ( route ) => {
+            return ( route.name.toLowerCase().includes( self.searchValue ) );
+          } );
+
+        else
+          return []
+      }
     },
+
     mounted() {
       
     },
@@ -48,7 +79,3 @@
     }
   }
 </script>
-
-<style>
-
-</style>
